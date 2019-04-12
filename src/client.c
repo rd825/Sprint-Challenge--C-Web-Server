ecb52478f3d1db758858bf1ecaffb26f7e5b31ac
Rahul Desai <rd825@georgetown.edu>
2019-04-12 11:57:18 -0400

    Experiencing segfaults connecting to remote server

--- src/client.c
+++ src/client.c
@@ -35,6 +35,24 @@ urlinfo_t *parse_url(char *url)
 
   urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
 
+  // Trying to parse HTTPS or HTTP from url; use strstr to get pointer to substring
+  char *https = strstr(hostname, "https://");
+  if (https != NULL)
+  {
+    hostname = https + strlen("https://");
+    *https = '\0';
+  }
+  else
+  {
+    char *http = strstr(hostname, "http://");
+
+    if (http != NULL)
+    {
+      hostname = http + strlen("http://");
+      *http = '\0';
+    }
+  }
+
   /*
     We can parse the input URL by doing the following:
 
@@ -52,13 +70,22 @@ urlinfo_t *parse_url(char *url)
     6. Overwrite the colon with a '\0' so that we are just left with the hostname.
   */
   char *colon = strchr(hostname, ':');
-  port = colon + 1;
-  *colon = '\0';
+
+  // enabling default port 80
+  if (colon == NULL)
+  {
+    port = "80";
+    urlinfo->port = port;
+  }
+  else
+  {
+    port = colon + 1;
+    *colon = '\0';
+    urlinfo->port = port;
+  }
 
   urlinfo->hostname = hostname;
-  urlinfo->port = port;
   urlinfo->path = path;
-
   return urlinfo;
 }
 
@@ -80,12 +107,14 @@ int send_request(int fd, char *hostname, char *port, char *path)
 
   sprintf(request, "GET /%s HTTP/1.1\n"
                    "Host: %s:%s\n"
-                   "Connection: close",
+                   "Connection: close\n\n",
           path, hostname, port);
+
+  printf("PRINT REQ: %s\n", request); // printing for my own sake
 
   rv = send(fd, request, strlen(request), 0);
 
-  if (rv == -1)
+  if (rv < 0)
   {
     perror("ERROR: could not send request.");
   }
@@ -113,16 +142,21 @@ int main(int argc, char *argv[])
   // 3. Call `send_request` to construct the request and send it
   int rv = send_request(sockfd, urlinfo->hostname, urlinfo->port, urlinfo->path);
 
+  if (rv == -1)
+  {
+    perror("ERROR: could not send request.");
+  }
+
   // 4. Call `recv` in a loop until there is no more data to receive from the server. Print the received response to stdout.
   while ((numbytes = recv(sockfd, buf, BUFSIZE - 1, 0)) > 0)
   {
     printf("%s\n", buf);
   }
-  close(sockfd);
 
   //   5. Clean up any allocated memory and open file descriptors.
   free(urlinfo->hostname);
   free(urlinfo);
+  close(sockfd);
 
   return 0;
 }
